7b926fbf840cad7b64b01de5974c7dd109e9ebe7
yefweb <yeferson.dev@gmail.com>
2023-01-18 20:24:17 -0500

    accent hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,6 +6,11 @@ import Button from './Button';
 const Hero = () => {
   return (
     <section id='inicio' className='mb-16 pt-[90px] '>
+      <div className='mt-3 inline-flex flex-row items-center gap-4 rounded-lg bg-gradient-to-tr from-primary to-cyan px-4 py-2'>
+        <span className='dot inline-block' />
+        <p className=''>¡Bienvenido a mi sitio web profesional!</p>
+      </div>
+
       <div className='py-6 md:flex md:flex-wrap'>
         <article className='mb-6 flex flex-col gap-6  md:mb-0 md:w-1/2'>
           <div>
