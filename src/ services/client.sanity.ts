83ce63a3a75a316d28b209da6ca78b9f421444ed
yefweb <yeferson.dev@gmail.com>
2023-02-10 10:20:46 -0500

    Sanity client API Version

--- src/ services/client.sanity.ts
+++ src/ services/client.sanity.ts
@@ -4,7 +4,7 @@ import imageUrl from '@sanity/image-url';
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  apiVersion: '2023-02-10',
   useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
 });
